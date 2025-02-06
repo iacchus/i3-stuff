6aa83900523615b38e0003e8ac7bb7153e22b6c8
Kassius <19712842+kassius@users.noreply.github.com>
2025-02-06 19:24:44 -0300

    commit

---
+++ swaywm-003.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import itertools
+import os
+import sys
+
+import i3ipc
+
+argc = len(sys.argv)
+
+if argc == 2 and sys.argv[1] in ("prev", "next"):
+    direction = sys.argv[1]
+else:
+    print(f"usage: {sys.argv[0]} <prev|next>")
+    exit(1)
+
+SWAYSOCK = os.environ['SWAYSOCK']
+
+sway = i3ipc.Connection(socket_path=SWAYSOCK)
+
+root = sway.get_tree()
+
+focused = root.find_focused()
+windows = root.descendants()
+
+focused_id = focused.id
+window_id_list = [window.id for window in windows
+                  if window.type in ("con", "floating_con")]
+
+if direction == "prev":
+    window_id_list.reverse()
+
+window_gen = itertools.cycle(window_id_list)
+
+for window_id in window_gen:
+    print("LOOP:", window_id)
+    if window_id == focused_id:
+        break
+
+focus_to_id = next(window_gen)
+
+print("GO TO:", focus_to_id)
