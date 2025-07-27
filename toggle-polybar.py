f4824a50ada27b3d7e77e7a087e9563f9ecb430c
ingstudio <117122269+ingeniumstudio@users.noreply.github.com>
2025-07-27 14:12:32 -0300

    commit

--- toggle-polybar.py
+++ toggle-polybar.py
@@ -11,28 +11,28 @@ import sys
 
 import i3ipc
 
-argc = len(sys.argv)
+NAME = "polybar"
+#  argc = len(sys.argv)
 
-if argc > 1:
-    command = sys.argv[1:]
-else:
-    print(f"usage: {sys.argv[0]} <command> [args]")
-    exit(1)
+#  if argc > 1:
+#      command = sys.argv[1:]
+#  else:
+#      print(f"usage: {sys.argv[0]} <command> [args]")
+#      exit(1)
 
 #  I3SOCK = os.environ['I3SOCK']
 
-#  sway = i3ipc.Connection(socket_path=I3SOCK)
 sway = i3ipc.Connection()
 
 for proc in psutil.process_iter():
 
-    if proc.name == sys.argv[1]:
-        subprocess.run(args=["killall", sys.argv[1]])
+    if proc.name == NAME:
+        subprocess.run(args=["killall", NAME])
         exit(0)
 
 #  command = ["swaymsg", f"[con_id={focus_to_id}]", "focus"]
 #  sway.command(f"[con_id={focus_to_id}] focus")
 #  sway.command(command)
-subprocess.run(args=command)
+subprocess.run(args=NAME)
 
 #  subprocess.run(args=command)
